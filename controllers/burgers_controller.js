5c45c9842d0deb63ce63bd4897f9793c7e72e9a1
Pane828 <pane828@gmail.com>
2017-09-06 19:56:25 -0400

    Add files via upload

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function(req, res) {
+  burger.create([
+    req.body.name, req.body.sleepy
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+});
+
+module.exports = router;
